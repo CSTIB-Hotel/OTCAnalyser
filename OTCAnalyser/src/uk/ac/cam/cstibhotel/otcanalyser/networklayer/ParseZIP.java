eefd132d252a556512233061e773c99be911efba
sztankatt <sztankatt@gmail.com>
2015-02-03 12:09:07 +0000

    Adding ParseZIP class, currently with one method, which can read a csv file.

---
+++ OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/networklayer/ParseZIP.java
@@ -0,0 +1,43 @@
+package uk.ac.cam.cstibhotel.otcanalyser.networklayer;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+/*
+ * 
+ * @author: ts579
+ * 
+ */
+
+public class ParseZIP {
+	
+	public void parseCSV(String file, String splitBy){
+		String line;
+		
+		try {
+	 		BufferedReader br = new BufferedReader(new FileReader(file));
+	 		while((line = br.readLine()) != null){
+	 			String[] trade = line.split(splitBy);
+	 			
+	 			System.out.println(trade[0]);
+	 		}
+	 		
+	 		br.close();
+		}
+		catch(FileNotFoundException e){
+			e.printStackTrace();
+		}
+		catch(IOException ioe){
+			ioe.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
