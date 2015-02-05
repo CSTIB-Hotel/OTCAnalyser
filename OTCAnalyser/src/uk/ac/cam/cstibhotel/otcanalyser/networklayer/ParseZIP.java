fcbac744f4d215b8040b57be3c801d13397fbee7
Tamas Sztanka-Toth <sztankatt@gmail.com>
2015-02-05 18:21:43 +0000

    New function responsible for translating one line to Trade object

--- OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/networklayer/ParseZIP.java
+++ OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/networklayer/ParseZIP.java
@@ -1,14 +1,15 @@
 package uk.ac.cam.cstibhotel.otcanalyser.networklayer;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.zip.ZipEntry;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.zip.ZipInputStream;
 import java.net.URL;
+
+import uk.ac.cam.cstibhotel.otcanalyser.trade.*;
 
 
 /*
@@ -19,24 +20,36 @@ import java.net.URL;
 
 public class ParseZIP {
 	
-	//TODO: Translate the csv to Trade objects
-	public static void zipToCsv(String zipFile, String splitBy){
+	//TODO: implement this bit
+	private static Trade stringVectorToTrade(String[] trade){
+		Trade test = null;
+		return test;
+	}
+	
+	public static LinkedList<Trade> downloadData(String zipFile, String splitBy){
 		String line;
+		int i = 0;
+		LinkedList<Trade> dataOut = new LinkedList<Trade>();
+		
 		try{
 			URL url = new URL(zipFile);
 			ZipInputStream zis = new ZipInputStream(url.openStream());
 			zis.getNextEntry();
 			InputStreamReader isr = new InputStreamReader(zis);
 			BufferedReader br = new BufferedReader(isr);
+			
+			//reading line by line
 			while((line = br.readLine()) != null){
-	 			String[] trade = line.split(splitBy);
-	 			for(String a : trade){
-	 				System.out.print(a+":");
+	 			//do not read the first line
+				if(i!=0){
+	 				dataOut.add(stringVectorToTrade(line.split(splitBy)));
 	 			}
-	 			System.out.println("");
+	 			i++;
 	 		}
 	 		
 	 		br.close();
+	 		
+	 		return dataOut;
 		}
 		catch(FileNotFoundException e){
 			e.printStackTrace();
@@ -45,10 +58,11 @@ public class ParseZIP {
 			ioe.printStackTrace();
 		}
 		
+		return null;
 	}
 	
 	public static void main(String[] args) {
-		ParseZIP.zipToCsv("https://kgc0418-tdw-data-0.s3.amazonaws.com/slices/SLICE_COMMODITIES_2015_02_05_569.zip",",");
+		ParseZIP.downloadData("https://kgc0418-tdw-data-0.s3.amazonaws.com/slices/CUMULATIVE_COMMODITIES_2015_02_04.zip",",");
 	}
 
 }
