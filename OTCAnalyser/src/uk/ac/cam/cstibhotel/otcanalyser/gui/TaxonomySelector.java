ee2becf6d018dea8ce49fb9721b3bf432b0269bb
Alistair Fisher <alistairstfisher@gmail.com>
2015-02-05 14:06:30 +0000

    Added unfinished version of search bar

---
+++ OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/gui/TaxonomySelector.java
@@ -0,0 +1,33 @@
+package uk.ac.cam.cst1bhotel.otcanalyser.gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JComboBox;
+import javax.swing.JInternalFrame;
+
+public class TaxonomySelector extends JInternalFrame {
+	private static final long serialVersionUID = 1L;
+	private JComboBox<String> Asset;
+	private JComboBox<String> BaseClass;
+	private JComboBox<String> SubClass;
+	
+	public TaxonomySelector() {
+		setTitle("Taxonomy Selector");
+		setSize(300,50); // default size is 0,0
+		setLocation(100,200); // default is 0,0 (top left corner)
+		Asset = new JComboBox<String>(TextStrings.Assets);
+		BaseClass = new JComboBox<String>(TextStrings.CommodityBaseProducts);
+		SubClass = new JComboBox<String>(TextStrings.CommodityMetalsSubProducts);
+		Asset.setVisible(true);
+		BaseClass.setVisible(true);
+		SubClass.setVisible(true);
+		this.getContentPane().add(Asset,BorderLayout.WEST);
+		this.getContentPane().add(BaseClass);
+		this.getContentPane().add(SubClass,BorderLayout.EAST);
+	}
+	
+	public static void main(String[] args) {
+		JInternalFrame f = new TaxonomySelector();
+		f.setVisible(true);
+	}
+}
