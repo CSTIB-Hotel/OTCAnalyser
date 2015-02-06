6625eab22f1143f82dc38cbf889f98a58c5b6037
sztankatt <sztankatt@gmail.com>
2015-02-06 17:27:03 +0000

    Added new exception types

--- OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/trade/BooleanFieldFormatException.java
+++ OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/trade/BooleanFieldFormatException.java
@@ -1,27 +1,27 @@
 package uk.ac.cam.cstibhotel.otcanalyser.trade;
 
-public class TradeFieldFormatException extends Exception {
+public class BooleanFieldFormatException extends Exception {
 
-	public TradeFieldFormatException() {
-		super();
+	public BooleanFieldFormatException() {
+		// TODO Auto-generated constructor stub
 	}
 
-	public TradeFieldFormatException(String message) {
+	public BooleanFieldFormatException(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
 	}
 
-	public TradeFieldFormatException(Throwable cause) {
+	public BooleanFieldFormatException(Throwable cause) {
 		super(cause);
 		// TODO Auto-generated constructor stub
 	}
 
-	public TradeFieldFormatException(String message, Throwable cause) {
+	public BooleanFieldFormatException(String message, Throwable cause) {
 		super(message, cause);
 		// TODO Auto-generated constructor stub
 	}
 
-	public TradeFieldFormatException(String message, Throwable cause,
+	public BooleanFieldFormatException(String message, Throwable cause,
 			boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
 		// TODO Auto-generated constructor stub
