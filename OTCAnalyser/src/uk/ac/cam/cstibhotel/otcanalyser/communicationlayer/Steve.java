70cc1d3c08748ef3140a4dad9bb3047f19334b8d
Gellert Weisz <gellert@raspberrypi.org>
2015-02-06 02:17:23 +0000

    add Steve, the entry point

---
+++ OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/communicationlayer/Steve.java
@@ -0,0 +1,10 @@
+package uk.ac.cam.cstibhotel.otcanalyser.communicationlayer;
+
+public class Steve {
+
+	public static void main(String[] args) {
+		//main entry point for the application
+
+	}
+
+}
