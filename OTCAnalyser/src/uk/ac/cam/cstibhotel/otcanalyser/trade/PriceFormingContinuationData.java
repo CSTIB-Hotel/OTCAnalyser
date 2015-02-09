2671561aac50a700db7dceaab43dea1884577588
sztankatt <sztankatt@gmail.com>
2015-02-09 15:41:50 +0000

    added AMENDMENT(2) to the enum

--- OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/trade/PriceFormingContinuationData.java
+++ OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/trade/PriceFormingContinuationData.java
@@ -3,7 +3,8 @@ package uk.ac.cam.cstibhotel.otcanalyser.trade;
 public enum PriceFormingContinuationData {
 
 	TERMINATION(0),
-	TRADE(1);
+	TRADE(1),
+	AMENDMENT(2);
 
 	private short value;
 
@@ -13,6 +14,21 @@ public enum PriceFormingContinuationData {
 
 	public short getValue() {
 		return value;
+	}
+	
+	public static PriceFormingContinuationData parsesPFCD(String s) throws PFCDFormatException{
+		switch(s){
+			case "Termination":
+				return TERMINATION;
+			case "Trade":
+				return TRADE;
+			case "Amendment":
+				return AMENDMENT;
+			case "":
+				return null;
+			default:
+				throw new PFCDFormatException();
+		}
 	}
 
 }
