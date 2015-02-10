00e6118d0ad23c94c88b2718985b8bd181756670
Sean Saville <sean.saville@hotmail.co.uk>
2015-02-10 10:10:35 +0000

    Updated UPI unit test, fixed UPI class to pass test

--- OTCAnalyser/test/uk/ac/cam/cstibhotel/otcanalyser/trade/UPITest.java
+++ OTCAnalyser/test/uk/ac/cam/cstibhotel/otcanalyser/trade/UPITest.java
@@ -12,9 +12,9 @@ public class UPITest {
 	
 	// Test a valid commodity taxonomy
 	@Test
-	public void testValidCommodityTaxonomy() throws InvalidTaxonomyException {
+	public void testValidCommodityTaxonomy() throws InvalidTaxonomyException, EmptyTaxonomyException {
 		UPI toTest = new UPI("Commodity:Metals:Precious:SpotFwd:Physical");
-		assertEquals(toTest.assetClass, AssetClass.Commodity);
+		assertEquals(toTest.assetClass, AssetClass.COMMODITY);
 		assertEquals(toTest.baseProduct, "Metals");
 		assertEquals(toTest.subProduct, "Precious");
 		assertEquals(toTest.transactionType, "SpotFwd");
@@ -23,9 +23,10 @@ public class UPITest {
 	
 	// Test a valid credit taxonomy
 	@Test
-	public void testValidCreditTaxonomy() throws InvalidTaxonomyException {
+	public void testValidCreditTaxonomy() throws InvalidTaxonomyException,
+			EmptyTaxonomyException {
 		UPI toTest = new UPI("Credit:SingleName:Corporate:StandardEuropeanCorporate");
-		assertEquals(toTest.assetClass, AssetClass.Credit);
+		assertEquals(toTest.assetClass, AssetClass.CREDIT);
 		assertEquals(toTest.baseProduct, "SingleName");
 		assertEquals(toTest.subProduct, "Corporate");
 		assertEquals(toTest.transactionType, "StandardEuropeanCorporate");
@@ -34,9 +35,10 @@ public class UPITest {
 	
 	// Test a valid equity taxonomy
 	@Test
-	public void testValidEquityTaxonomy() throws InvalidTaxonomyException {
+	public void testValidEquityTaxonomy() throws InvalidTaxonomyException,
+			EmptyTaxonomyException {
 		UPI toTest = new UPI("Equity:PortfolioSwap:PriceReturnBasicPerformance:SingleName");
-		assertEquals(toTest.assetClass, AssetClass.Equity);
+		assertEquals(toTest.assetClass, AssetClass.EQUITY);
 		assertEquals(toTest.baseProduct, "PortfolioSwap");
 		assertEquals(toTest.subProduct, "PriceReturnBasicPerformance");
 		assertEquals(toTest.transactionType, "SingleName");
@@ -45,9 +47,10 @@ public class UPITest {
 	
 	// Test a valid foreign exchange taxonomy (with a sub-product)
 	@Test
-	public void testValidForeignExchangeTaxonomyWithSubProduct() throws InvalidTaxonomyException {
+	public void testValidForeignExchangeTaxonomyWithSubProduct() throws InvalidTaxonomyException,
+			EmptyTaxonomyException {
 		UPI toTest = new UPI("ForeignExchange:SimpleExotic:IntradayDigital");
-		assertEquals(toTest.assetClass, AssetClass.ForeignExchange);
+		assertEquals(toTest.assetClass, AssetClass.FOREX);
 		assertEquals(toTest.baseProduct, "SimpleExotic");
 		assertEquals(toTest.subProduct, "IntradayDigital");
 		assertEquals(toTest.transactionType, null);
@@ -57,9 +60,9 @@ public class UPITest {
 	// Test a valid foreign exchange taxonomy (without a sub-product)
 	@Test
 	public void testValidForeignExchangeTaxonomyWithoutSubProduct() 
-			throws InvalidTaxonomyException {
+			throws InvalidTaxonomyException, EmptyTaxonomyException {
 		UPI toTest = new UPI("ForeignExchange:NDF");
-		assertEquals(toTest.assetClass, AssetClass.ForeignExchange);
+		assertEquals(toTest.assetClass, AssetClass.FOREX);
 		assertEquals(toTest.baseProduct, "NDF");
 		assertEquals(toTest.subProduct, null);
 		assertEquals(toTest.transactionType, null);
@@ -68,9 +71,10 @@ public class UPITest {
 	
 	// Test a valid interest rate taxonomy (with a sub-product)
 	@Test
-	public void testValidRatesTaxonomyWithSubProduct() throws InvalidTaxonomyException {
+	public void testValidRatesTaxonomyWithSubProduct() throws InvalidTaxonomyException,
+			EmptyTaxonomyException {
 		UPI toTest = new UPI("InterestRate:CrossCurrency:Basis");
-		assertEquals(toTest.assetClass, AssetClass.Rates);
+		assertEquals(toTest.assetClass, AssetClass.RATES);
 		assertEquals(toTest.baseProduct, "CrossCurrency");
 		assertEquals(toTest.subProduct, "Basis");
 		assertEquals(toTest.transactionType, null);
@@ -79,9 +83,10 @@ public class UPITest {
 	
 	// Test a valid interest rate taxonomy (without a sub-product)
 	@Test
-	public void testValidRatesTaxonomyWithoutSubProduct() throws InvalidTaxonomyException {
+	public void testValidRatesTaxonomyWithoutSubProduct() throws InvalidTaxonomyException,
+			EmptyTaxonomyException {
 		UPI toTest = new UPI("InterestRate:CapFloor");
-		assertEquals(toTest.assetClass, AssetClass.Rates);
+		assertEquals(toTest.assetClass, AssetClass.RATES);
 		assertEquals(toTest.baseProduct, "CapFloor");
 		assertEquals(toTest.subProduct, null);
 		assertEquals(toTest.transactionType, null);
@@ -90,84 +95,96 @@ public class UPITest {
 	
 	// Test a taxonomy with an invalid asset class - expect an exception
 	@Test(expected = InvalidTaxonomyException.class)
-	public void testInvalidAssetClassTaxonomy() throws InvalidTaxonomyException {
+	public void testInvalidAssetClassTaxonomy() throws InvalidTaxonomyException, 
+			EmptyTaxonomyException {
 		@SuppressWarnings("unused")
 		UPI toTest = new UPI("Fail:Metals:Precious:SpotFwd:Physical");
 	}
 	
 	// Test an empty taxonomy - expect an exception
-	@Test(expected = InvalidTaxonomyException.class)
-	public void testEmptyTaxonomy() throws InvalidTaxonomyException {
+	@Test(expected = EmptyTaxonomyException.class)
+	public void testEmptyTaxonomy() throws EmptyTaxonomyException, 
+			InvalidTaxonomyException {
 		@SuppressWarnings("unused")
 		UPI toTest = new UPI("");
 	}
 	
 	// Test an invalid commodity taxonomy (not enough fields) - expect an exception
 	@Test(expected = InvalidTaxonomyException.class)
-	public void testInvalidShortCommodityTaxonomy() throws InvalidTaxonomyException {
+	public void testInvalidShortCommodityTaxonomy() throws InvalidTaxonomyException, 
+			EmptyTaxonomyException {
 		@SuppressWarnings("unused")
 		UPI toTest = new UPI("Commodity:Metals:Precious");
 	}
 	
 	// Test an invalid commodity taxonomy (too many fields) - expect an exception
 	@Test(expected = InvalidTaxonomyException.class)
-	public void testInvalidLongCommodityTaxonomy() throws InvalidTaxonomyException {
+	public void testInvalidLongCommodityTaxonomy() throws InvalidTaxonomyException, 
+			EmptyTaxonomyException {
 		@SuppressWarnings("unused")
 		UPI toTest = new UPI("Commodity:Metals:Precious:SpotFwd:Physical:Fail");
 	}
 	
 	// Test an invalid credit taxonomy (not enough fields) - expect an exception
 	@Test(expected = InvalidTaxonomyException.class)
-	public void testInvalidShortCreditTaxonomy() throws InvalidTaxonomyException {
+	public void testInvalidShortCreditTaxonomy() throws InvalidTaxonomyException,
+			EmptyTaxonomyException {
 		@SuppressWarnings("unused")
 		UPI toTest = new UPI("Credit:SingleName:Corporate");
 	}
 	
 	// Test an invalid credit taxonomy (too many fields) - expect an exception
 	@Test(expected = InvalidTaxonomyException.class)
-	public void testInvalidLongCreditTaxonomy() throws InvalidTaxonomyException {
+	public void testInvalidLongCreditTaxonomy() throws InvalidTaxonomyException,
+			EmptyTaxonomyException {
 		@SuppressWarnings("unused")
 		UPI toTest = new UPI("Credit:SingleName:Corporate:StandardEuropeanCorporate:Fail");
 	}
 	
 	// Test an invalid equity taxonomy (not enough fields) - expect an exception
 	@Test(expected = InvalidTaxonomyException.class)
-	public void testInvalidShortEquityTaxonomy() throws InvalidTaxonomyException {
+	public void testInvalidShortEquityTaxonomy() throws InvalidTaxonomyException, 
+			EmptyTaxonomyException {
 		@SuppressWarnings("unused")
 		UPI toTest = new UPI("Equity:PortfolioSwap:PriceReturnBasicPerformance");
 	}
 	
 	// Test an invalid equity taxonomy (too many fields) - expect an exception
 	@Test(expected = InvalidTaxonomyException.class)
-	public void testInvalidLongEquityTaxonomy() throws InvalidTaxonomyException {
+	public void testInvalidLongEquityTaxonomy() throws InvalidTaxonomyException, 
+			EmptyTaxonomyException {
 		@SuppressWarnings("unused")
 		UPI toTest = new UPI("Equity:PortfolioSwap:PriceReturnBasicPerformance:SingleName:Fail");
 	}
 	
 	// Test an invalid foreign exchange taxonomy (not enough fields) - expect an exception
 	@Test(expected = InvalidTaxonomyException.class)
-	public void testInvalidShortForeignExchangeTaxonomy() throws InvalidTaxonomyException {
+	public void testInvalidShortForeignExchangeTaxonomy() throws InvalidTaxonomyException,
+			EmptyTaxonomyException {
 		@SuppressWarnings("unused")
 		UPI toTest = new UPI("ForeignExchange");
 	}
 	
 	// Test an invalid foreign exchange taxonomy (too many fields) - expect an exception
 	@Test(expected = InvalidTaxonomyException.class)
-	public void testInvalidLongForeignExchangeTaxonomy() throws InvalidTaxonomyException {
+	public void testInvalidLongForeignExchangeTaxonomy() throws InvalidTaxonomyException,
+			EmptyTaxonomyException {
 		@SuppressWarnings("unused")
 		UPI toTest = new UPI("ForeignExchange:SimpleExotic:IntradayDigital:Fail");
 	}
 	
 	// Test an invalid rates taxonomy (not enough fields) - expect an exception
 	@Test(expected = InvalidTaxonomyException.class)
-	public void testInvalidShortRatesTaxonomy() throws InvalidTaxonomyException {
+	public void testInvalidShortRatesTaxonomy() throws InvalidTaxonomyException,
+			EmptyTaxonomyException {
 		@SuppressWarnings("unused")
 		UPI toTest = new UPI("InterestRate");
 	}
 	
 	// Test an invalid rates taxonomy (too many fields) - expect an exception
 	@Test(expected = InvalidTaxonomyException.class)
-	public void testInvalidLongRatesTaxonomy() throws InvalidTaxonomyException {
+	public void testInvalidLongRatesTaxonomy() throws InvalidTaxonomyException,
+			EmptyTaxonomyException {
 		@SuppressWarnings("unused")
 		UPI toTest = new UPI("InterestRate:CrossCurrency:Basis:Fail");
 	}
