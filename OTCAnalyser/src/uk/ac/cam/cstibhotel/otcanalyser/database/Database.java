b15ba1adbac9ba884dba77dfa2db661c86991a54
waiwaing <waiwai95@gmail.com>
2015-02-10 11:46:32 +0000

    Cleaned up database imports

--- OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/database/Database.java
+++ OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/database/Database.java
@@ -15,8 +15,6 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
