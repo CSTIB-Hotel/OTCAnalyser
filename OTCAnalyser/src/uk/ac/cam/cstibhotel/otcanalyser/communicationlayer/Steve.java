385cc486f9449575612502c3349568431ca475cb
Sean Saville <sean.saville@hotmail.co.uk>
2015-02-13 11:16:47 +0000

    Fixes #14 (hopefully) by forcing constructor of communication layer

--- OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/communicationlayer/Steve.java
+++ OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/communicationlayer/Steve.java
@@ -7,6 +7,7 @@ public class Steve {
 
 	public static void main(String[] args) {
 		//main entry point for the application
+		CommunicationLayer.getInstance();
 		GUI.getInstance();
 		NetworkLayer.initialUpdate();
 		CommunicationLayer.registerListener(GUI.getInstance());
