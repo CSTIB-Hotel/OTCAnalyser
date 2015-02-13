5fb4d0619435f304d8f46845904fb17669cdfb18
Alistair Fisher <alistairstfisher@gmail.com>
2015-02-13 15:56:36 +0000

    Created SaveWindowClass, changed CenteredJLabel from inner class to class in its own right

--- OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/gui/SaveWindow.java
+++ OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/gui/SaveWindow.java
@@ -1,5 +1,6 @@
 package uk.ac.cam.cstibhotel.otcanalyser.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 
 import javax.swing.BoxLayout;
@@ -9,69 +10,60 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class SearchWindow extends JPanel {
+public class SaveWindow extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
-	private JLabel TaxonomySelectorDescriptor;
-	public TaxonomySelector tax;
-	private JLabel UnderLyingAssetDescriptor;
-	public JTextField UnderLyingAsset;
-	private JLabel StartDateDescriptor;
-	public DateSelector StartDate;
-	private JLabel EndDateDescriptor;
-	public DateSelector EndDate;
-	private JLabel TradeTypeDescriptor;
-	public JComboBox<String> TradeType;
-	private JLabel minValueDescriptor;
-	public JTextField minValue;
-	private JLabel maxValueDescriptor;
-	public JTextField maxValue;
-	private JLabel currencyDescriptor;
-	public JTextField currency;
-	public JButton SearchButton;
+	private JLabel WindowLabel;
+	private JLabel TextboxLabel;
+	public JTextField Name;
+	public JButton SaveButton;
+	//private SaveButtonListener listener;
 	
-	private static SearchWindow instance;
+	private static SaveWindow instance;
 	
-	public static SearchWindow getInstance() {
-		if (instance==null) instance = new SearchWindow();
+	public static SaveWindow getInstance() {
+		if (instance==null) instance = new SaveWindow();
 		return instance;
 	}
 	
-	private SearchWindow() {
+	private SaveWindow() {
 			setSize(200,100); // default size is 0,0
 			setLocation(100,200); // default is 0,0 (top left corner)
-			setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-			TaxonomySelectorDescriptor =  new CenteredJLabel("Select a taxonomy");
-			this.add(TaxonomySelectorDescriptor);
-			TaxonomySelectorDescriptor.setVisible(true);
-			tax = TaxonomySelector.getInstance();
-			this.add(tax);
-			tax.setVisible(true);
-			UnderLyingAssetDescriptor = new CenteredJLabel("Underlying Asset (optional)");
-			this.add(UnderLyingAssetDescriptor);
-			UnderLyingAssetDescriptor.setVisible(true);
-			UnderLyingAsset = new JTextField();
+			
+			WindowLabel =  new CenteredJLabel("Save a Trade");
+			this.add(WindowLabel,BorderLayout.NORTH);
+			WindowLabel.setVisible(true);
+			
+			TextboxLabel = new CenteredJLabel("Name");
+			this.add(TextboxLabel);
+			TextboxLabel.setVisible(true);
+			
+			Name = new JTextField();
 			this.add(UnderLyingAsset);
 			UnderLyingAsset.setVisible(true);
 			TradeTypeDescriptor = new CenteredJLabel("Select trade type");
+
 			this.add(TradeTypeDescriptor);
 			TradeTypeDescriptor.setVisible(true);
 			TradeType = new JComboBox<String>(TextStrings.TradeType);
 			this.add(TradeType);
 			TradeType.setVisible(true);
 			StartDateDescriptor = new CenteredJLabel("Select start date");
+
 			this.add(StartDateDescriptor);
 			StartDateDescriptor.setVisible(true);
 			StartDate = new DateSelector();
 			this.add(StartDate);
 			StartDate.setVisible(true);
 			EndDateDescriptor = new CenteredJLabel("Select end date");
+
 			this.add(EndDateDescriptor);
 			EndDateDescriptor.setVisible(true);
 			EndDate = new DateSelector();
 			this.add(EndDate);
 			EndDate.setVisible(true);
 			minValueDescriptor = new CenteredJLabel("Select minimum price");
+
 			this.add(minValueDescriptor);
 			minValueDescriptor.setVisible(true);
 			minValue = new JTextField();
@@ -93,6 +85,8 @@ public class SearchWindow extends JPanel {
 			SearchButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 			this.add(SearchButton);
 			SearchButton.setVisible(true);
+			listener = new SearchButtonListener();
+			SearchButton.addActionListener(listener);
 	}
 	
 }
