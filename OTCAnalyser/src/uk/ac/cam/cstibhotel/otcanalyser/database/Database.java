251b9702e5b001af4b42dadf941fd2a115c51893
waiwaing <waiwai95@gmail.com>
2015-02-17 12:55:20 +0000

    Add a blank line

--- OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/database/Database.java
+++ OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/database/Database.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
