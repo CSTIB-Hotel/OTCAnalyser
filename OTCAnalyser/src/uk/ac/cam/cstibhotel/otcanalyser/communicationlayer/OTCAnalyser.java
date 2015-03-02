ac7bbfd39c0245c33190779f605b92c0d54b8cf7
Sean Saville <sean.saville@hotmail.co.uk>
2015-03-02 11:26:02 +0000

    RIP Steve

--- OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/communicationlayer/OTCAnalyser.java
+++ OTCAnalyser/src/uk/ac/cam/cstibhotel/otcanalyser/communicationlayer/OTCAnalyser.java
@@ -3,7 +3,7 @@ package uk.ac.cam.cstibhotel.otcanalyser.communicationlayer;
 import uk.ac.cam.cstibhotel.otcanalyser.gui.GUI;
 import uk.ac.cam.cstibhotel.otcanalyser.networklayer.NetworkLayer;
 
-public class Steve {
+public class OTCAnalyser {
 
 	public static void main(String[] args) {
 		//main entry point for the application
